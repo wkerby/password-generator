cb02cc96c063cc17790f2a64baae898849baa59d
William Kerby <w.kerby99@gmail.com>
2023-01-11 02:22:13 -0500

    add user prompt for password length

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,11 @@
 // Assignment code here
+var password = "";
+//to what data type does js store prompt variables? going to assume it is string value and convert to string to int using parseInt
+var numchars = parseInt(prompt("Please provide password length (must be no fewer than 8 characters and no greater than 128 characters)."));
+if (numchars < 8 || numchars > 128) {
+  console.log("Please enter an integer value between 8 and 128")
+}
+
 
 
 // Get references to the #generate element
@@ -15,3 +22,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
