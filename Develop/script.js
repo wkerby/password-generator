0be275504c2730cae0a4fa2dba465b4c13cf8a14
William Kerby <w.kerby99@gmail.com>
2023-01-15 11:58:30 -0500

    fix program so that no re-prompt occurs if not all character types for which user opts are in first randomly generated password

--- Develop/script.js
+++ Develop/script.js
@@ -5,37 +5,38 @@ function generatePassword() {
 
   var active = true; //create while loop that verifies that all characters that the user opted for are in the password
 
-  while (active) {
-    //create password variable that function will return
-    var password = "";
+  //create password variable that function will return
+  var password = "";
 
-    //create array to capture all variations of a True type response from user
-    var yesResponses = ["yes", "Yes", "YES", "y"];
+  //create array to capture all variations of a True type response from user
+  var yesResponses = ["yes", "Yes", "YES", "y"];
 
-    //create object in which all user prompted boolean responses will be stored
-    var userBools = { Lower: ["lowercase", ""], Upper: ["uppercase", ""], Numeric: ["numeric", ""], Special: ["special", ""], };
+  //create object in which all user prompted boolean responses will be stored
+  var userBools = { Lower: ["lowercase", ""], Upper: ["uppercase", ""], Numeric: ["numeric", ""], Special: ["special", ""], };
 
-    //prompt user to provide password length
-    var numchars = parseInt(prompt("Please provide password length (must be no fewer than 8 characters and no greater than 128 characters)."));
+  //prompt user to provide password length
+  var numchars = parseInt(prompt("Please provide password length (must be no fewer than 8 characters and no greater than 128 characters)."));
 
-    //validate that user input is within required range of 8 and 128 characters
-    if (numchars < 8 || numchars > 128) {
-      console.log("Please enter an integer value between 8 and 128");
+  //validate that user input is within required range of 8 and 128 characters
+  if (numchars < 8 || numchars > 128) {
+    console.log("Please enter an integer value between 8 and 128");
+  }
+
+  //loop over all questions pertaining to password characteristics 
+  for (var i = 0; i < Object.values(userBools).length; i++) {
+
+    if (yesResponses.includes(prompt("Include " + Object.values(userBools)[i][0] + " characters (y/n)?"))) { //consider using the confirm built-in function
+      Object.values(userBools)[i][1] = true;
     }
-
-    //loop over all questions pertaining to password characteristics 
-    for (var i = 0; i < Object.values(userBools).length; i++) {
-
-      if (yesResponses.includes(prompt("Include " + Object.values(userBools)[i][0] + " characters (y/n)?"))) { //consider using the confirm built-in function
-        Object.values(userBools)[i][1] = true;
-      }
-      else {
-        Object.values(userBools)[i][1] = false;
-      }
+    else {
+      Object.values(userBools)[i][1] = false;
     }
+  }
 
-    //validate that user has opted for at least one character type
-    if (Object.values(userBools)[2][1] == false && Object.values(userBools)[3][1] == false) {
+  while (active) { //validate that user has opted for at least one character type
+
+
+    if (Object.values(userBools)[0][1] == false && Object.values(userBools)[1][1] == false) {
       console.log("You must choose at least one character type");
     }
 
@@ -147,10 +148,6 @@ function generatePassword() {
 
   return password;
 }
-
-
-
-
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
